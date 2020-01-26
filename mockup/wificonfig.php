964021cb7b17e4722a1ab3fe139d0ec49b60eaa0
Alex Villacís Lasso <a_villacis@palosanto.com>
2020-01-26 17:49:01 -0500

    Mockup PHP para escaneo de redes wifi
    rsync -avr mockup/ root@elx4-2.elastix.palosanto.com:/var/www/html/yubox-mockup/

---
+++ mockup/wificonfig.php
@@ -0,0 +1,58 @@
+<?php
+
+Header('Content-Type: application/json');
+
+if (!isset($_SERVER['PATH_INFO'])) {
+    Header('HTTP/1.1 400 Bad Request');
+    print json_encode('Se requiere indicar ruta');
+    exit();
+}
+
+switch ($_SERVER['PATH_INFO']) {
+    case '/mac':
+        $output = $retval = NULL;
+        exec('/usr/sbin/ifconfig eth0', $output, $retval);
+        if ($retval != 0) {
+            Header('HTTP/1.1 500 Internal Server Error');
+            print json_encode($output);
+            exit();
+        }
+        $regs = NULL;
+        foreach ($output as $s) if (preg_match('/ether (\S+)/', $s, $regs)) {
+            print json_encode($regs[1]);
+            break;
+        }
+        break;
+    case '/scan':
+        sleep(2);   // Simular retraso en escaneo
+        if (file_exists('/tmp/wifiscan.json')) {
+            $scan = json_decode(file_get_contents('/tmp/wifiscan.json'), TRUE);
+        } else {
+            srand(time());
+            $scan = array();
+            for ($i = 0; $i < 20; $i++) {
+                $scan[] = array(
+                    'bssid'     =>  sprintf('00:11:00:11:00:%02x', $i),
+                    'ssid'      =>  sprintf('RED-PRUEBA-%02d', $i),
+                    'channel'   =>  rand(0, 11),
+                    'rssi'      =>  rand(-100, 0),
+                    'authmode'  =>  rand(0, 6),
+                );
+            }
+        }
+        for ($i = 0; $i < count($scan); $i++) {
+            $rssi = $scan[$i]['rssi'] + rand(-5, 5);
+            if ($rssi > 0) $rssi = 0;
+            if ($rssi < -100) $rssi = -100;
+            $scan[$i]['rssi'] = $rssi;
+        }
+        $json = json_encode($scan);
+        file_put_contents('/tmp/wifiscan.json', $json);
+        print $json;
+        break;
+    default:
+        Header('HTTP/1.1 404 Not Found');
+        print json_encode('Ruta no implementada');
+        exit();
+}
+
