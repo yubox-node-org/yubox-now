82e246e60a3e288f1485eb8cc2a2a4101db39634
Alex Villacís Lasso <a_villacis@palosanto.com>
2020-05-02 20:21:31 -0500

    (WIP) Yubox OTA: implementación de flasheo del firmware desde tar.gz
    Se afina la detección del fin del tar en vista de caso observado.
    Se implementa reporte de resultado de actualización desde manejador POST
    FALTA: reiniciar luego de aplicar exitosamente firmware
    FALTA: manejar archivos a copiar a SPIFFS
    FALTA: hacer accesible rollback de firmware

--- src/YuboxOTAClass.cpp
+++ src/YuboxOTAClass.cpp
@@ -12,6 +12,8 @@ extern "C" {
   #include "TinyUntar/untar.h" // https://github.com/dsoprea/TinyUntar
 }
 
+#include <Update.h>
+
 #define GZIP_DICT_SIZE 32768
 #define GZIP_BUFF_SIZE 4096
 #ifndef SPI_FLASH_SEC_SIZE
@@ -36,10 +38,10 @@ YuboxOTAClass::YuboxOTAClass(void)
   _gz_dstdata = NULL;
   _gz_dict = NULL;
   _gz_actualExpandedSize = 0;
-  _tarCB.header_cb = _tar_cb_gotEntryHeader;
-  _tarCB.data_cb = _tar_cb_gotEntryData;
-  _tarCB.end_cb = _tar_cb_gotEntryEnd;
-  tinyUntarReadCallback = _tar_cb_feedFromBuffer;
+  _tarCB.header_cb = ::_tar_cb_gotEntryHeader;
+  _tarCB.data_cb = ::_tar_cb_gotEntryData;
+  _tarCB.end_cb = ::_tar_cb_gotEntryEnd;
+  tinyUntarReadCallback = ::_tar_cb_feedFromBuffer;
 }
 
 void YuboxOTAClass::begin(AsyncWebServer & srv)
@@ -104,6 +106,13 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
     _tar_emptyChunk = 0;
     _tar_eof = false;
     tar_setup(&_tarCB, this);
+
+    // Inicialización de estado de actualización
+    _tgzupload_clientError = false;
+    _tgzupload_serverError = false;
+    _tgzupload_responseMsg = "";
+    _tgzupload_currentOp = OTA_IDLE;
+    _tgzupload_foundFirmware = false;
   }
 
   // Agregar búfer recibido al búfer de entrada, notando si debe empezarse a parsear gzip
@@ -113,6 +122,8 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
   if (2 * GZIP_BUFF_SIZE - used < len) {
     // No hay suficiente espacio para este bloque de datos. ESTO NO DEBERÍA PASAR
     Serial.printf("ERR: no hay suficiente espacio en _gz_srcdata: libre=%u requerido=%u\r\n", 2 * GZIP_BUFF_SIZE - used, len);
+    _tgzupload_serverError = true;
+    _tgzupload_responseMsg = "(internal) Falta espacio en búfer para siguiente pedazo de datos!";
     _uploadRejected = true;
   } else {
     unsigned long gz_expectedExpandedSize = 0;
@@ -126,6 +137,8 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
       // el tamaño esperado de longitud expandida.
       if (used < 4) {
         Serial.printf("ERR: no hay suficientes datos luego de bloque final para determinar tamaño expandido, se tienen %u bytes\r\n", used);
+        _tgzupload_clientError = true;
+        _tgzupload_responseMsg = "Archivo es demasiado corto para validar longitud gzip";
         _uploadRejected = true;
       } else {
         gz_expectedExpandedSize =
@@ -136,6 +149,8 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
         //Serial.printf("DEBUG: longitud esperada de datos expandidos es %lu bytes\r\n", gz_expectedExpandedSize);
         if (gz_expectedExpandedSize < _gz_actualExpandedSize) {
           Serial.printf("ERR: longitud ya expandida excede longitud esperada! %lu > %lu\r\n", _gz_actualExpandedSize, gz_expectedExpandedSize);
+          _tgzupload_clientError = true;
+          _tgzupload_responseMsg = "Longitud esperada inconsistente con datos ya expandidos";
           _uploadRejected = true;
         }
       }
@@ -152,6 +167,8 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
         if (r != TINF_OK) {
           // Fallo al parsear la cabecera gzip
           Serial.println("ERR: fallo al parsear cabecera gzip");
+          _tgzupload_clientError = true;
+          _tgzupload_responseMsg = "Archivo no parece ser un archivo tar.gz, o está corrupto";
           _uploadRejected = true;
           break;
         }
@@ -166,6 +183,8 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
         r = uzlib_uncompress(&_uzLib_decomp);
         if (r != TINF_DONE && r != TINF_OK) {
           Serial.printf("ERR: fallo al descomprimir gzip (err=%d)\r\n", r);
+          _tgzupload_clientError = true;
+          _tgzupload_responseMsg = "Archivo corrupto o truncado (gzip), no puede descomprimirse";
           _uploadRejected = true;
           break;
         }
@@ -178,15 +197,14 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
         while (!_tar_eof && !_uploadRejected && _tar_offset < gzExpandedSize) {
           // _tar_offset se actualiza en _tar_cb_feedFromBuffer()
           // Procesamiento continúa en callbacks _tar_cb_*
-          r = read_tar_step();
-          if (r == -1 && _tar_emptyChunk >= 2 && gz_expectedExpandedSize != 0 && _gz_actualExpandedSize >= gz_expectedExpandedSize) {
+          r = _tar_eof ? 0 : read_tar_step();
+          if (r == -1 && _tar_emptyChunk >= 2 && gz_expectedExpandedSize != 0) {
             _tar_eof = true;
-            Serial.printf("DEBUG: se alcanzó el final del tar actual=%lu expected=%lu\r\n", _gz_actualExpandedSize, gz_expectedExpandedSize);
-
-            // TODO: condición de fin de datos subidos
-
+            //Serial.printf("DEBUG: se alcanzó el final del tar actual=%lu esperado=%lu\r\n", _gz_actualExpandedSize, gz_expectedExpandedSize);
           } else if (r != 0) {
-            Serial.printf("ERR: fallo al procesar tar en bloque offset %u error %d emptychunk %d\r\n", _tar_offset, r, _tar_emptyChunk);
+            Serial.printf("ERR: fallo al procesar tar en bloque offset %u error %d emptychunk %d actual=%lu esperado=%lu\r\n", _tar_offset, r, _tar_emptyChunk, _gz_actualExpandedSize, gz_expectedExpandedSize);
+            _tgzupload_clientError = true;
+            _tgzupload_responseMsg = "Archivo corrupto o truncado (tar), no puede procesarse";
             _uploadRejected = true;
           }
         }
@@ -209,6 +227,10 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
     }
   }
 
+  if (!_uploadRejected && _tar_eof) {
+    // TODO: condición de fin de datos subidos
+  }
+
   if (_uploadRejected || final) {
     tar_abort("tar cleanup", 0);
     if (_gz_dict != NULL) { delete _gz_dict; _gz_dict = NULL; }
@@ -218,50 +240,71 @@ void YuboxOTAClass::_handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len,
   }
 }
 
-int _tar_cb_feedFromBuffer(unsigned char * buf, size_t size)
+int YuboxOTAClass::_tar_cb_feedFromBuffer(unsigned char * buf, size_t size)
 {
-  Serial.printf("DEBUG: _tar_cb_feedFromBuffer buf=%p size=%d\r\n", buf, size);
-  // Ya que no hay un puntero a contexto, este método está obligado a accesar
-  // a YuboxOTA directamente
-  YuboxOTAClass * ota = &YuboxOTA;
-
   if (size % TAR_BLOCK_SIZE != 0) {
     Serial.printf("ERR: _tar_cb_feedFromBuffer: longitud pedida %d no es múltiplo de %d\r\n", size, TAR_BLOCK_SIZE);
     return 0;
   }
 
-  ota->_tar_emptyChunk++;
+  _tar_emptyChunk++;
 
-  unsigned int gzExpandedSize = ota->_uzLib_decomp.dest - ota->_uzLib_decomp.dest_start;
-  unsigned int copySize = gzExpandedSize - ota->_tar_offset;
-  Serial.printf("DEBUG: _tar_cb_feedFromBuffer máximo copia posible %d bytes...\r\n", copySize);
+  unsigned int gzExpandedSize = _uzLib_decomp.dest - _uzLib_decomp.dest_start;
+  unsigned int copySize = gzExpandedSize - _tar_offset;
+  //Serial.printf("DEBUG: _tar_cb_feedFromBuffer máximo copia posible %d bytes...\r\n", copySize);
   if (copySize > size) copySize = size;
-  Serial.printf("DEBUG: _tar_cb_feedFromBuffer copiando %d bytes...\r\n", copySize);
-  memcpy(buf, ota->_uzLib_decomp.dest_start + ota->_tar_offset, copySize);
+  //Serial.printf("DEBUG: _tar_cb_feedFromBuffer copiando %d bytes...\r\n", copySize);
+  memcpy(buf, _uzLib_decomp.dest_start + _tar_offset, copySize);
 
   // Si no hay suficientes datos para llenar size, se rellena con 0xFF
   if (copySize < size) {
     Serial.printf("WARN: _tar_cb_feedFromBuffer: se piden %d bytes pero sólo se pueden proveer %d bytes\r\n",
       size, copySize);
     memset(buf + copySize, 0xff, size - copySize);
-    ota->_tar_offset += copySize;
+    _tar_offset += copySize;
     return size;
   } else  {
-    ota->_tar_offset += copySize;
+    _tar_offset += copySize;
     return copySize;
   }
 }
 
-int _tar_cb_gotEntryHeader(header_translated_t * hdr, int entry_index, void * context_data)
+int YuboxOTAClass::_tar_cb_gotEntryHeader(header_translated_t * hdr, int entry_index)
 {
-  YuboxOTAClass * ota = (YuboxOTAClass *)context_data;
+  static const char * const firmwareSuffix = ".ino.nodemcu-32s.bin";
 
-  Serial.printf("DEBUG: _tar_cb_gotEntryHeader: %s INICIO\r\n", hdr->filename);
+  //Serial.printf("DEBUG: _tar_cb_gotEntryHeader: %s INICIO\r\n", hdr->filename);
   switch (hdr->type)
   {
   case T_NORMAL:
-    Serial.printf("DEBUG: archivo ordinario tamaño 0x%08x%08x\r\n", (unsigned long)(hdr->filesize >> 32), (unsigned long)(hdr->filesize & 0xFFFFFFFFUL));
+    //Serial.printf("DEBUG: archivo ordinario tamaño 0x%08x%08x\r\n", (unsigned long)(hdr->filesize >> 32), (unsigned long)(hdr->filesize & 0xFFFFFFFFUL));
+
+    // Verificar si el archivo que se presenta es un firmware a flashear. El firmware debe
+    // tener un nombre que termine en ".ino.nodemcu-32s.bin" . Este es el valor por omisión
+    // con el que se termina el nombre del archivo compilado exportado por Arduino IDE
+    unsigned int fnLen = strlen(hdr->filename);
+    if (fnLen > strlen(firmwareSuffix) && 0 == strcmp(hdr->filename + (fnLen - strlen(firmwareSuffix)), firmwareSuffix)) {
+      //Serial.printf("DEBUG: detectado firmware: %s longitud %d bytes\r\n", hdr->filename, (unsigned long)(hdr->filesize & 0xFFFFFFFFUL));
+      if (_tgzupload_foundFirmware) {
+        Serial.printf("WARN: se ignora firmware duplicado: %s longitud %d bytes\r\n", hdr->filename, (unsigned long)(hdr->filesize & 0xFFFFFFFFUL));
+      } else {
+        _tgzupload_foundFirmware = true;
+        if ((unsigned long)(hdr->filesize >> 32) != 0) {
+          // El firmware excede de 4GB. Un dispositivo así de grande no existe
+          _tgzupload_serverError = true;
+          _tgzupload_responseMsg = _updater_errstr(UPDATE_ERROR_SIZE);
+          _uploadRejected = true;
+        } else if (!Update.begin(hdr->filesize, U_FLASH)) {
+          _tgzupload_serverError = true;
+          _tgzupload_responseMsg = _updater_errstr(Update.getError());
+          _uploadRejected = true;
+        } else {
+          _tgzupload_currentOp = OTA_FIRMWARE_FLASH;
+        }
+      }
+    }
     break;
+/*
   case T_HARDLINK:       Serial.printf("Ignoring hard link to %s.\r\n", hdr->filename); break;
   case T_SYMBOLIC:       Serial.printf("Ignoring sym link to %s.\r\n", hdr->filename); break;
   case T_CHARSPECIAL:    Serial.printf("Ignoring special char.\r\n"); break;
@@ -272,33 +315,129 @@ int _tar_cb_gotEntryHeader(header_translated_t * hdr, int entry_index, void * co
   case T_GLOBALEXTENDED: Serial.printf("Ignoring global extended data.\r\n"); break;
   case T_EXTENDED:       Serial.printf("Ignoring extended data.\r\n"); break;
   case T_OTHER: default: Serial.printf("Ignoring unrelevant data.\r\n");       break;
-
+*/
   }
-  ota->_tar_emptyChunk = 0;
+  _tar_emptyChunk = 0;
 
-  return ota->_uploadRejected ? -1 : 0;
+  return _uploadRejected ? -1 : 0;
+}
+
+int YuboxOTAClass::_tar_cb_gotEntryData(header_translated_t * hdr, int entry_index, unsigned char * block, int size)
+{
+  //Serial.write(block, size);
+  switch (_tgzupload_currentOp) {
+  case OTA_SPIFFS_WRITE:
+    // TODO: implementar
+    break;
+  case OTA_FIRMWARE_FLASH:
+    while (size > 0) {
+      size_t r = Update.write(block, size);
+      if (r == 0) {
+        _tgzupload_serverError = true;
+        _tgzupload_responseMsg = _updater_errstr(Update.getError());
+        _uploadRejected = true;
+        Update.abort();
+        _tgzupload_currentOp = OTA_IDLE;
+        break;
+      }
+      size -= r;
+      block += r;
+    }
+    break;
+  }
+
+  _tar_emptyChunk = 0;
+  return _uploadRejected ? -1 : 0;
+}
+
+int YuboxOTAClass::_tar_cb_gotEntryEnd(header_translated_t * hdr, int entry_index)
+{
+  //Serial.printf("\r\nDEBUG: _tar_cb_gotEntryEnd: %s FINAL\r\n", hdr->filename);
+  switch (_tgzupload_currentOp) {
+  case OTA_SPIFFS_WRITE:
+    _tgzupload_currentOp = OTA_IDLE;
+    // TODO: implementar
+    break;
+  case OTA_FIRMWARE_FLASH:
+    _tgzupload_currentOp = OTA_IDLE;
+    if (!Update.end()) {
+      _tgzupload_serverError = true;
+      _tgzupload_responseMsg = _updater_errstr(Update.getError());
+      _uploadRejected = true;
+    } else if (!Update.isFinished()) {
+      _tgzupload_serverError = true;
+      _tgzupload_responseMsg = "Actualización no ha podido finalizarse: ";
+      _tgzupload_responseMsg += _updater_errstr(Update.getError());
+      _uploadRejected = true;
+    }
+    break;
+  }
+
+  _tar_emptyChunk = 0;
+  return _uploadRejected ? -1 : 0;
+}
+
+const char * YuboxOTAClass::_updater_errstr(uint8_t e)
+{
+  switch (e) {
+  case UPDATE_ERROR_OK:
+    return "(no hay error)";
+  case UPDATE_ERROR_WRITE:
+    return "Fallo al escribir al flash";
+  case UPDATE_ERROR_ERASE:
+    return "Fallo al borrar bloque flash";
+  case UPDATE_ERROR_READ:
+    return "Fallo al leer del flash";
+  case UPDATE_ERROR_SPACE:
+    return "No hay suficiente espacio en búfer para actualización";
+  case UPDATE_ERROR_SIZE:
+    return "Firmware demasiado grande para flash de dispositivo";
+  case UPDATE_ERROR_STREAM:
+    return "Error de lectura en stream fuente";
+  case UPDATE_ERROR_MD5:
+    return "Error en checksum MD5";
+  case UPDATE_ERROR_MAGIC_BYTE:
+    return "Firma incorrecta en firmware";
+  case UPDATE_ERROR_ACTIVATE:
+    return "Error en activación de firmware actualizado";
+  case UPDATE_ERROR_NO_PARTITION:
+    return "No puede localizarse partición para escribir firmware";
+  case UPDATE_ERROR_BAD_ARGUMENT:
+    return "Argumemto incorrecto";
+  case UPDATE_ERROR_ABORT:
+    return "Actualización abortada";
+  default:
+    return "(error desconocido)";
+  }
+}
+
+int _tar_cb_feedFromBuffer(unsigned char * buf, size_t size)
+{
+  //Serial.printf("DEBUG: _tar_cb_feedFromBuffer buf=%p size=%d\r\n", buf, size);
+  // Ya que no hay un puntero a contexto, este método está obligado a accesar
+  // a YuboxOTA directamente
+  YuboxOTAClass * ota = &YuboxOTA;
+  return ota->_tar_cb_feedFromBuffer(buf, size);
+}
+
+int _tar_cb_gotEntryHeader(header_translated_t * hdr, int entry_index, void * context_data)
+{
+  YuboxOTAClass * ota = (YuboxOTAClass *)context_data;
+  return ota->_tar_cb_gotEntryHeader(hdr, entry_index);
 }
 
 int _tar_cb_gotEntryData(header_translated_t * hdr, int entry_index, void * context_data, unsigned char * block, int size)
 {
   YuboxOTAClass * ota = (YuboxOTAClass *)context_data;
+  return ota->_tar_cb_gotEntryData(hdr, entry_index, block, size);
 
-  Serial.write(block, size);
-
-  ota->_tar_emptyChunk = 0;
-  return ota->_uploadRejected ? -1 : 0;
 }
 
 int _tar_cb_gotEntryEnd(header_translated_t * hdr, int entry_index, void * context_data)
 {
   YuboxOTAClass * ota = (YuboxOTAClass *)context_data;
-
-  Serial.printf("\r\nDEBUG: _tar_cb_gotEntryEnd: %s FINAL\r\n", hdr->filename);
-
-  ota->_tar_emptyChunk = 0;
-  return ota->_uploadRejected ? -1 : 0;
+  return ota->_tar_cb_gotEntryEnd(hdr, entry_index);
 }
-
 
 void YuboxOTAClass::_routeHandler_yuboxAPI_yuboxOTA_tgzupload_POST(AsyncWebServerRequest * request)
 {
@@ -311,15 +450,37 @@ void YuboxOTAClass::_routeHandler_yuboxAPI_yuboxOTA_tgzupload_POST(AsyncWebServe
     return (request)->requestAuthentication();
   }
 
-  Serial.println("DEBUG: tgzupload_POST a punto de revisar existencia de parámetro");
-  if (request->hasParam("tgzupload", true, true)) {
-    Serial.println("DEBUG: tgzupload_POST tgzupload SÍ existe como upload");
-  } else {
-    Serial.println("DEBUG: tgzupload_POST tgzupload NO existe como upload");
+  bool clientError = false;
+  bool serverError = false;
+  String responseMsg = "";
+
+  if (!request->hasParam("tgzupload", true, true)) {
+    clientError = true;
+    responseMsg = "No se ha especificado archivo de actualización.";
+  }
+
+  if (!clientError) clientError = _tgzupload_clientError;
+  if (!serverError) serverError = _tgzupload_serverError;
+  if (responseMsg == "") responseMsg = _tgzupload_responseMsg;
+
+  if (!clientError && !serverError) {
+    responseMsg = "Firmware actualizado correctamente. El equipo se reiniciará en unos momentos.";
   }
 
   _uploadRejected = false;
-  request->send(500, "application/json", "{\"msg\":\"DEBUG: Carga de actualización no implementado\"}");
+
+  unsigned int httpCode = 200;
+  if (clientError) httpCode = 400;
+  if (serverError) httpCode = 500;
+
+  AsyncResponseStream *response = request->beginResponseStream("application/json");
+  response->setCode(httpCode);
+  DynamicJsonDocument json_doc(JSON_OBJECT_SIZE(2));
+  json_doc["success"] = !(clientError || serverError);
+  json_doc["msg"] = responseMsg.c_str();
+
+  serializeJson(json_doc, *response);
+  request->send(response);
 }
 
 YuboxOTAClass YuboxOTA;
--- src/YuboxOTAClass.h
+++ src/YuboxOTAClass.h
@@ -9,6 +9,13 @@ extern "C" {
   #include "TinyUntar/untar.h" // https://github.com/dsoprea/TinyUntar
 }
 
+typedef enum
+{
+  OTA_IDLE,           // Código ocioso, o el archivo está siendo ignorado
+  OTA_SPIFFS_WRITE,   // Se está escribiendo el archivo a SPIFFS
+  OTA_FIRMWARE_FLASH  // Se está escribiendo a flash de firmware
+} YuboxOTA_operationWithFile;
+
 class YuboxOTAClass
 {
 private:
@@ -29,6 +36,15 @@ private:
   unsigned int _tar_emptyChunk;         // Número de bloques de 512 bytes llenos de ceros contiguos
   bool _tar_eof;                        // Bandera de si se llegó a fin normal de tar
 
+  // Descripción de errores posibles al subir el tar.gz
+  bool _tgzupload_clientError;
+  bool _tgzupload_serverError;
+  String _tgzupload_responseMsg;
+
+  // Operación de actualización en sí
+  YuboxOTA_operationWithFile _tgzupload_currentOp;
+  bool _tgzupload_foundFirmware;
+
   void _setupHTTPRoutes(AsyncWebServer &);
 
   void _routeHandler_yuboxAPI_yuboxOTA_tgzupload_POST(AsyncWebServerRequest *);
@@ -36,6 +52,14 @@ private:
     String filename, size_t index, uint8_t *data, size_t len, bool final);
 
   void _handle_tgzOTAchunk(size_t index, uint8_t *data, size_t len, bool final);
+
+  const char * _updater_errstr(uint8_t);
+
+  // Las siguientes funciones son llamadas por la correspondiente friend del mismo nombre
+  int _tar_cb_feedFromBuffer(unsigned char *, size_t);
+  int _tar_cb_gotEntryHeader(header_translated_t *, int);
+  int _tar_cb_gotEntryData(header_translated_t *, int, unsigned char *, int);
+  int _tar_cb_gotEntryEnd(header_translated_t *, int);
 
 public:
   YuboxOTAClass(void);
