0e22e2cf4d43110d0b47d89111d4e560d5f1cf71
Alex Villacís Lasso <a_villacis@palosanto.com>
2020-05-04 16:57:12 -0500

    Actualización de estado de carga de firmware a través de SSE

--- data-template/yuboxOTA/yuboxapp.js
+++ data-template/yuboxOTA/yuboxapp.js
@@ -1,6 +1,10 @@
 function setupYuboxOTATab()
 {
     var otapane = $('div#yuboxMainTabContent > div.tab-pane#yuboxOTA');
+    var data = {
+        'sse':  null
+    };
+    otapane.data(data);
 
     otapane.find('input[type=file]#tgzupload').change(function () {
         var lbl = $(this).next('label.custom-file-label');
@@ -25,6 +29,7 @@ function setupYuboxOTATab()
         }
         var postData = new FormData();
         postData.append('tgzupload', fi[0].files[0]);
+        yuboxOTAUpload_init();
         $.post({
             url: yuboxAPI('yuboxOTA')+'/tgzupload',
             data: postData,
@@ -41,6 +46,7 @@ function setupYuboxOTATab()
             } else {
                 yuboxMostrarAlertText('danger', data.msg, 2000);
             }
+            yuboxOTAUpload_shutdown();
         })
         .fail(function (e) {
             var msg;
@@ -52,6 +58,71 @@ function setupYuboxOTATab()
                 msg = e.responseJSON.msg;
             }
             yuboxMostrarAlertText('danger', msg, 2000);
+            yuboxOTAUpload_shutdown();
         });
     });
+}
+
+function yuboxOTAUpload_init()
+{
+    var otapane = $('div#yuboxMainTabContent > div.tab-pane#yuboxOTA');
+    otapane.find('button[name=apply]').prop('disabled', true);
+    yuboxOTAUpload_setProgressBar(0);
+    otapane.find('div.upload-progress span#filename').text('-');
+    otapane.find('div.upload-progress span#current').text('0');
+    otapane.find('div.upload-progress span#total').text('0');
+    otapane.find('div.upload-progress span#currupload').text('0');
+    otapane.find('div.upload-progress').show();
+
+    if (!!window.EventSource) {
+        var sse = new EventSource(yuboxAPI('yuboxOTA')+'/events');
+        sse.addEventListener('uploadFileStart', function (e) {
+            var data = $.parseJSON(e.data);
+            var totalKB = data.total / 1024.0;
+            otapane.find('div.upload-progress span#filename').text(data.filename);
+            otapane.find('div.upload-progress span#current').text(0);
+            otapane.find('div.upload-progress span#total').text(totalKB.toFixed(1));
+            otapane.find('div.progress-bar')
+                .removeClass('bg-info bg-danger')
+                .addClass(data.firmware ? 'bg-danger' : 'bg-info');
+            yuboxOTAUpload_setProgressBar(0);
+        });
+        sse.addEventListener('uploadFileProgress', function (e) {
+            var data = $.parseJSON(e.data);
+            var totalKB = data.total / 1024.0;
+            var currKB = data.current / 1024.0;
+            otapane.find('div.upload-progress span#filename').text(data.filename);
+            otapane.find('div.upload-progress span#total').text(totalKB.toFixed(1));
+            otapane.find('div.upload-progress span#current').text(currKB.toFixed(1));
+            yuboxOTAUpload_setProgressBar(totalKB > 0.0 ? 100.0 * currKB / totalKB : 0);
+        });
+        sse.addEventListener('uploadFileEnd', function (e) {
+            var data = $.parseJSON(e.data);
+            var totalKB = data.total / 1024.0;
+            otapane.find('div.upload-progress span#filename').text(data.filename);
+            otapane.find('div.upload-progress span#current').text(totalKB.toFixed(1));
+            otapane.find('div.upload-progress span#total').text(totalKB.toFixed(1));
+            yuboxOTAUpload_setProgressBar(100);
+        });
+        otapane.data('sse', sse);
+    }
+}
+
+function yuboxOTAUpload_shutdown()
+{
+    var otapane = $('div#yuboxMainTabContent > div.tab-pane#yuboxOTA');
+    otapane.find('button[name=apply]').prop('disabled', false);
+    otapane.find('div.upload-progress').hide();
+    if (otapane.data('sse') != null) {
+        otapane.data('sse').close();
+        otapane.data('sse', null);
+    }
+}
+
+function yuboxOTAUpload_setProgressBar(v)
+{
+    var pb = $('div#yuboxMainTabContent > div.tab-pane#yuboxOTA div.progress-bar');
+    pb.css('width', v+'%');
+    pb.attr('aria-valuenow', v);
+    pb.text(v.toFixed(1) + ' %');
 }
