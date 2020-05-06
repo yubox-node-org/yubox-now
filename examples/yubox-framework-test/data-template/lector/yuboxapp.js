293d72a5c39a7d85a0f4ddc1d71b4d7dc560d979
Alex Villacís Lasso <a_villacis@palosanto.com>
2020-05-06 11:32:08 -0500

    yubox-framework-test: implementación de ejemplo interesante de lector
    Parte javascript con mockup que simula salida de Server-Sent Events.

---
+++ examples/yubox-framework-test/data-template/lector/yuboxapp.js
@@ -0,0 +1,81 @@
+function setupLectorTab()
+{
+    var lectorpane = $('div#yuboxMainTabContent > div.tab-pane#lector');
+    var data = {
+        'sse':  null,
+        'chart': null,
+    };
+    lectorpane.data(data);
+
+    var ctx = lectorpane.find('canvas#atmospheric')[0].getContext('2d');
+    var chtData = {
+        datasets:   [{
+            label:  'Presión',
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgb(255, 99, 132)",
+            fill: false,
+            data: [],
+            yAxisID: 'press'
+        },{
+            label:  'Temperatura',
+            borderColor: "rgb(54, 162, 235)",
+            backgroundColor: "rgb(54, 162, 235)",
+            fill: false,
+            data: [],
+            yAxisID: 'temp'
+        }]
+    };
+    var cht = new Chart(ctx, {
+        type:       'line',
+        data:       chtData,
+        options:    {
+            responsive: true,
+            hoverMode: 'index',
+            stacked: false,
+            title: {
+                display: true,
+                text: 'Presión y temperatura'
+            },
+            scales: {
+                xAxes:  [{
+                    type:   'time'
+                }],
+                yAxes:  [{
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    id: 'press'
+                },{
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    id: 'temp'
+                }]
+            }
+        }
+    });
+    lectorpane.data('chart', cht);
+
+    if (!!window.EventSource) {
+        var sse = new EventSource(yuboxAPI('lectura'));
+        sse.addEventListener('message', function (e) {
+            var data = $.parseJSON(e.data);
+            yuboxLector_actualizar(new Date(data.ts), data.pressure, data.temperature);
+        });
+    }
+}
+
+function yuboxLector_actualizar(d, p, t)
+{
+    var lectorpane = $('div#yuboxMainTabContent > div.tab-pane#lector');
+    lectorpane.find('h3#temp').text(t.toFixed(2));
+    lectorpane.find('h3#press').text(p.toFixed(2));
+    var cht = lectorpane.data('chart');
+    cht.data.datasets[0].data.push({x: d, y: p});
+    cht.data.datasets[1].data.push({x: d, y: t});
+    if (d.valueOf() - cht.data.datasets[0].data[0].x.valueOf() > 10 * 60 * 1000) {
+        cht.data.datasets[0].data.shift();
+        cht.data.datasets[1].data.shift();
+    }
+    cht.update();
+}
