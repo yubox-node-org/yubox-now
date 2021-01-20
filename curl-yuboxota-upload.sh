ee6b8bd0a966261ee4bbcd7239a6d1d506924b94
Alex Villacís Lasso <a_villacis@palosanto.com>
2021-01-19 20:32:12 -0500

    Utility script to send firmware update through command line
    Useful if an update has botched the Javascript code and the HTML
    interface is unusable.

---
+++ curl-yuboxota-upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOST=$1
+TGZ=$2
+CRED=admin:yubox
+
+curl -u "$CRED" -F tgzupload=@$TGZ  http://$HOST/yubox-api/yuboxOTA/tgzupload
+curl -u "$CRED" -d '' http://$HOST/yubox-api/yuboxOTA/reboot
