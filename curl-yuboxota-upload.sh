474dfd0b2095d24ef16b9f5a40d1f6ae98241c87
Alex Villacís Lasso <a_villacis@palosanto.com>
2021-05-20 18:05:20 -0500

    Actualizar script con nuevo URL de OTA

--- curl-yuboxota-upload.sh
+++ curl-yuboxota-upload.sh
@@ -4,5 +4,5 @@ HOST=$1
 TGZ=$2
 CRED=admin:yubox
 
-curl -u "$CRED" -F tgzupload=@$TGZ  http://$HOST/yubox-api/yuboxOTA/tgzupload
+curl -u "$CRED" -F tgzupload=@$TGZ  http://$HOST/yubox-api/yuboxOTA/esp32/tgzupload
 curl -u "$CRED" -d '' http://$HOST/yubox-api/yuboxOTA/reboot
