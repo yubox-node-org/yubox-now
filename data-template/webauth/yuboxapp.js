773289fc05e7eace59ccb94398aa3a7f5eb8215a
Alex Villacís Lasso <a_villacis@palosanto.com>
2021-11-30 18:26:37 -0500

    authconfig: conversión completa para dejar de usar jQuery
    A excepción del tab de navegación que es de Bootstrap 4, el resto del
    API ya no requiere jQuery para funcionar.

--- data-template/webauth/yuboxapp.js
+++ data-template/webauth/yuboxapp.js
@@ -1,30 +1,28 @@
 function setupWebAuthTab()
 {
-    //
-    var authpane = getYuboxPane('webauth');
+    const authpane = getYuboxPane('webauth', true);
 
     // https://getbootstrap.com/docs/4.4/components/navs/#events
     getYuboxNavTab('webauth').on('shown.bs.tab', function (e) {
-        var authpane = getYuboxPane('webauth');
-        $.getJSON(yuboxAPI('authconfig'))
-        .done(function (data) {
-            authpane.find('input#yubox_username').val(data.username);
-            authpane.find('input#yubox_password1, input#yubox_password2').val(data.password);
-        })
-        .fail(function (e) { yuboxStdAjaxFailHandler(e, 2000); });
+        yuboxFetch('authconfig')
+        .then((data) => {
+            authpane.querySelector('input#yubox_username').value = data.username;
+            authpane.querySelectorAll('input#yubox_password1, input#yubox_password2')
+            .forEach((yubox_password) => { yubox_password.value = data.password; });
+        }, (e) => { yuboxStdAjaxFailHandler(e, 2000); });
     });
-    authpane.find('button[name=apply]').click(function () {
+    authpane.querySelector('button[name=apply]').addEventListener('click', function () {
         var postData = {
-            password1: authpane.find('input#yubox_password1').val(),
-            password2: authpane.find('input#yubox_password2').val()
+            password1: authpane.querySelector('input#yubox_password1').value,
+            password2: authpane.querySelector('input#yubox_password2').value
         };
         if (postData.password1 != postData.password2) {
             yuboxMostrarAlertText('danger', 'Contraseña y confirmación no coinciden.', 5000);
         } else if (postData.password1 == '') {
             yuboxMostrarAlertText('danger', 'Contraseña no puede estar vacía.', 5000);
         } else {
-            $.post(yuboxAPI('authconfig'), postData)
-            .done(function (data) {
+            yuboxFetch('authconfig', '', postData)
+            .then((data) => {
                 if (data.success) {
                     // Al guardar correctamente las credenciales, recargar para que las pida
                     yuboxMostrarAlertText('success', data.msg, 2000);
@@ -34,8 +32,7 @@ function setupWebAuthTab()
                 } else {
                     yuboxMostrarAlertText('danger', data.msg, 2000);
                 }
-            })
-            .fail(function (e) { yuboxStdAjaxFailHandler(e, 2000); });
+            }, (e) => { yuboxStdAjaxFailHandler(e, 2000); })
         }
     });
 }
