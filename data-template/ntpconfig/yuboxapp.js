ab90af107fec2099b1c50445e2b5360f342975c3
Alex Villacís Lasso <a_villacis@palosanto.com>
2021-11-30 19:04:59 -0500

    ntpconfig: conversión completa para dejar de usar jQuery
    A excepción del tab de navegación que es de Bootstrap 4, el resto del
    API ya no requiere jQuery para funcionar.

--- data-template/ntpconfig/yuboxapp.js
+++ data-template/ntpconfig/yuboxapp.js
@@ -1,35 +1,35 @@
 function setupNTPConfTab()
 {
-    var ntppane = getYuboxPane('ntpconfig');
+    const ntppane = getYuboxPane('ntpconfig', true);
 
     // Llenar los select con valores para elegir zona horaria
-    var sel_tzh = ntppane.find('select#ntptz_hh');
-    var sel_tzm = ntppane.find('select#ntptz_mm');
+    const sel_tzh = ntppane.querySelector('select#ntptz_hh');
+    const sel_tzm = ntppane.querySelector('select#ntptz_mm');
     for (var i = -12; i <= 14; i++) {
-        $('<option></option>')
-            .val(i)
-            .text(((i >= 0) ? '+' : '-')+(("0"+Math.abs(i)).slice(-2)))
-            .appendTo(sel_tzh);
+        let opt = document.createElement('option');
+        opt.value = i;
+        opt.textContent = ((i >= 0) ? '+' : '-')+(("0"+Math.abs(i)).slice(-2));
+        sel_tzh.insertAdjacentElement('beforeend', opt);
     }
     for (var i = 0; i < 60; i++) {
-        $('<option></option>')
-            .val(i)
-            .text(("0"+Math.abs(i)).slice(-2))
-            .appendTo(sel_tzm);
+        let opt = document.createElement('option');
+        opt.value = i;
+        opt.textContent = ("0"+Math.abs(i)).slice(-2);
+        sel_tzm.insertAdjacentElement('beforeend', opt);
     }
-    ntppane.find('button[name=apply]').click(function () {
-        var ntppane = getYuboxPane('ntpconfig');
-        var sel_tzh = ntppane.find('select#ntptz_hh');
-        var sel_tzm = ntppane.find('select#ntptz_mm');
+    ntppane.querySelector('button[name=apply]').addEventListener('click', function () {
+        const ntppane = getYuboxPane('ntpconfig', true);
+        const sel_tzh = ntppane.querySelector('select#ntptz_hh');
+        const sel_tzm = ntppane.querySelector('select#ntptz_mm');
     
         var postData = {
-            ntphost:    ntppane.find('form input#ntphost').val(),
-            ntptz:      parseInt(sel_tzh.val()) * 3600
+            ntphost:    ntppane.querySelector('form input#ntphost').value,
+            ntptz:      parseInt(sel_tzh.value) * 3600
         };
-        postData.ntptz += ((postData.ntptz >= 0) ? 1 : -1) * parseInt(sel_tzm.val()) * 60;
+        postData.ntptz += ((postData.ntptz >= 0) ? 1 : -1) * parseInt(sel_tzm.value) * 60;
 
-        $.post(yuboxAPI('ntpconfig')+'/conf.json', postData)
-        .done(function (r) {
+        yuboxFetch('ntpconfig', 'conf.json', postData)
+        .then((r) => {
             if (r.success) {
                 // Recargar los datos recién guardados del dispositivo
                 yuboxMostrarAlertText('success', r.msg, 3000);
@@ -40,8 +40,7 @@ function setupNTPConfTab()
             } else {
                 yuboxMostrarAlertText('danger', r.msg);
             }
-        })
-        .fail(function (e) { yuboxStdAjaxFailHandler(e, 2000); });
+        }, (e) => { yuboxStdAjaxFailHandler(e, 2000); });
     });
 
     // https://getbootstrap.com/docs/4.4/components/navs/#events
@@ -53,44 +52,37 @@ function setupNTPConfTab()
 
 function yuboxLoadNTPConfig()
 {
-    var ntppane = getYuboxPane('ntpconfig');
-    ntppane.find('form span#ntp_connstatus')
-        .removeClass('badge-success badge-danger')
-        .addClass('badge-secondary')
-        .text('(consultando)');
-    ntppane.find('form span#ntp_timestamp').text('...');
+    const ntppane = getYuboxPane('ntpconfig', true);
+    const span_connstatus = ntppane.querySelector('form span#ntp_connstatus');
+    const span_timestamp = ntppane.querySelector('form span#ntp_timestamp');
 
-    $.get(yuboxAPI('ntpconfig')+'/conf.json')
-    .done(function (data) {
-        var sel_tzh = ntppane.find('select#ntptz_hh');
-        var sel_tzm = ntppane.find('select#ntptz_mm');
-        var span_connstatus = ntppane.find('form span#ntp_connstatus');
-        var span_timestamp = ntppane.find('form span#ntp_timestamp');
+    span_connstatus.classList.remove('badge-success', 'badge-danger');
+    span_connstatus.classList.add('badge-secondary');
+    span_connstatus.textContent = '(consultando)';
+    span_timestamp.textContent = '...';
 
-        span_connstatus.removeClass('badge-danger badge-success badge-secondary');
-        span_timestamp.text('');
+    yuboxFetch('ntpconfig', 'conf.json')
+    .then((data) => {
+        span_connstatus.classList.remove('badge-danger', 'badge-success', 'badge-secondary');
+        span_timestamp.textContent = '';
         if (data.ntpsync) {
-            span_connstatus
-                .addClass('badge-success')
-                .text('SINCRONIZADO');
-            //span_timestamp.text('TODO');
+            span_connstatus.classList.add('badge-success');
+            span_connstatus.textContent = 'SINCRONIZADO';
         } else {
-            span_connstatus
-                .addClass('badge-danger')
-                .text('NO SINCRONIZADO');
-            span_timestamp.text('No se ha contactado a servidor NTP');
+            span_connstatus.classList.add('badge-danger');
+            span_connstatus.textContent = 'NO SINCRONIZADO';
+            span_timestamp.textContent = 'No se ha contactado a servidor NTP';
         }
 
-        ntppane.find('form input#ntphost').val(data.ntphost);
-        sel_tzh.val(Math.trunc(data.ntptz / 3600));
-        sel_tzm.val(Math.trunc(Math.abs((data.ntptz % 3600) / 60)));
-    })
-    .fail(function (e) {
-        ntppane.find('form span#ntp_connstatus')
-            .removeClass('badge-success badge-danger badge-secondary')
-            .addClass('badge-danger')
-            .text('NO SINCRONIZADO');
-        ntppane.find('form span#ntp_timestamp').text('(error en consulta)');
+        ntppane.querySelector('form input#ntphost').value = data.ntphost;
+
+        ntppane.querySelector('select#ntptz_hh').value = (Math.trunc(data.ntptz / 3600));
+        ntppane.querySelector('select#ntptz_mm').value = (Math.trunc(Math.abs((data.ntptz % 3600) / 60)));
+    }, (e) => {
+        span_connstatus.classList.remove('badge-danger', 'badge-success', 'badge-secondary');
+        span_connstatus.classList.add('badge-danger');
+        span_connstatus.textContent = 'NO SINCRONIZADO';
+        span_timestamp.textContent = '(error en consulta)';
 
         yuboxStdAjaxFailHandler(e, 2000);
     });
