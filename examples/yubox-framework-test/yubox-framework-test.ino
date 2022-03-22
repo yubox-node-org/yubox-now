fcd46c9d1e9168d02ab34b50125ce0c2b7d2e4cd
Alex Villacís Lasso <a_villacis@palosanto.com>
2022-03-22 15:05:17 -0500

    Restaurar opcionalmente el uso de sensores reales en ejemplo framework
    Se documenta la manera de activar opcionalmente el uso de sensores
    reales a través del archivo build_opt.h . De lo contrario, se sigue
    usando un mockup usando random().

--- examples/yubox-framework-test/yubox-framework-test.ino
+++ examples/yubox-framework-test/yubox-framework-test.ino
@@ -1,6 +1,23 @@
 #include <YuboxSimple.h>
-
 #include <YuboxMQTTConfClass.h>
+
+/*
+Si se dispone de una placa con el sensor BMP280 conectado en el bus I2C, como
+el board original YUBOX, se puede activar el uso de este sensor de la siguiente
+manera:
+1) Se debe crear un archivo llamado build_opt.h en el directorio del proyecto
+2) El contenido del archivo debe contener en una línea la siguiente directiva:
+
+-DYUBOX_SENSOR_INTEGRADO=1
+
+ */
+#ifdef YUBOX_SENSOR_INTEGRADO
+// El modelo viejo de YUBOX tiene este sensor integrado en el board
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 sensor_bmp280;
+#endif
 
 AsyncEventSource eventosLector("/yubox-api/lectura/events");
 
@@ -16,7 +33,14 @@ void setup()
   YuboxMQTTConf.begin(yubox_HTTPServer);
 
   yuboxSimpleSetup();
-  
+
+#ifdef YUBOX_SENSOR_INTEGRADO
+  if (!sensor_bmp280.begin(BMP280_ADDRESS_ALT)) {
+    Serial.println("ERR: no puede inicializarse el sensor BMP280!");
+  }
+#else
+  Serial.println("Usando MOCKUP en lugar de un sensor válido.");
+#endif
 }
 
 void loop()
@@ -24,10 +48,28 @@ void loop()
   yuboxSimpleLoopTask();
 
   if (YuboxNTPConf.isNTPValid(0)) {
+    float temperature, pressure;
+#ifndef YUBOX_SENSOR_INTEGRADO
+    float last_temperature = 25.0f;
+    float last_pressure = 101325.0f;
+#endif
+
     DynamicJsonDocument json_doc(JSON_OBJECT_SIZE(3));
     json_doc["ts"] = 1000ULL * YuboxNTPConf.getUTCTime();
-    json_doc["temperature"] = random(1,100);
-    json_doc["pressure"] = random(1,100);
+#ifdef YUBOX_SENSOR_INTEGRADO
+    temperature = sensor_bmp280.readTemperature();
+    pressure = sensor_bmp280.readPressure();
+#else
+    temperature = last_temperature + random(-1000, 1000) / 1000.f;
+    pressure = last_pressure + random(-500, 500);
+    if (temperature < 10.0f) temperature = 10.0f;
+    if (temperature > 40.0f) temperature = 40.0f;
+    if (pressure < 100000.0f) pressure = 100000.0f;
+    if (pressure > 102000.0f) pressure = 102000.0f;
+#endif
+
+    json_doc["temperature"] = temperature;
+    json_doc["pressure"] = pressure;
 
     String json_output;
     serializeJson(json_doc, json_output);
